9d001c8659ba868d34b975f061ec8db9c0cfa532
Emmanuel Vidal <emmanuelv02@hotmail.com>
2017-03-06 00:59:32 -0400

    Complete implementing assignation 10

--- src/PHttp/HttpServer.cs
+++ src/PHttp/HttpServer.cs
@@ -1,93 +1,179 @@
 ﻿using System;
 using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace PHttp
 {
-    public class HttpServer : IDisposable
-    {
-        private const int WriteReadBufferSize = 4096;
-        private const int ShutdownTimeout = 30;
-        private const int WriteReadTimeOut = 90;
+	public class HttpServer : IDisposable
+	{
+		private const int WriteReadBufferSize = 4096;
+		private const int ShutdownTimeout = 30;
+		private const int WriteReadTimeOut = 90;
 
-        #region Properties
-        public IPEndPoint EndPoint { get; set; }
-        public int ReadBufferSize { get; set; }
-        public int WriteBufferSize { get; set; }
-        public string ServerBanner { get; set; }
-        public TimeSpan ReadTimeout { get; set; }
-        public TimeSpan WriteTimeout { get; set; }
-        public TimeSpan ShutdowTimeout { get; set; }
+		private TcpListener _tcpListener;
+		private AutoResetEvent _clientsChangedEvent = new AutoResetEvent (false);
+		private bool _disposed;
+		private HttpServerState _state = HttpServerState.Stopped;
 
-       // internal HttpServerUtility ServerUtility { get; set; }
-      //  internal HttpTimeoutManager TimeoutManager { get; set; }
-        #endregion
+		#region Properties
 
-        #region Constructor
-        public HttpServer()
-        {
-            EndPoint = new IPEndPoint(IPAddress.Loopback, 0);
-            ReadBufferSize = WriteReadBufferSize;
-            WriteBufferSize = WriteReadBufferSize;
-            ShutdowTimeout = TimeSpan.FromSeconds(ShutdownTimeout);
-            ReadTimeout = TimeSpan.FromMinutes(WriteReadTimeOut);
-            WriteTimeout = TimeSpan.FromMinutes(WriteReadTimeOut);
-            ServerBanner = string.Format("PHttp/{0}", GetType().Assembly.GetName().Version);
-        }
+		public EventHandler StateChanged{ get; set; }
 
-        #endregion
+		public HttpServerState State { 
+			get { return _state; } 
+			set {
+				if (value != _state) {
+					_state = value;
+					OnStateChanged (new EventArgs());
+				}
+			}
+		}
 
-        #region Public Methods
+		public IPEndPoint EndPoint { get; private set; }
+		public int Port{ get; private set;}
 
-        public void Start()
-        {
+		public int ReadBufferSize { get; set; }
 
-        }
+		public int WriteBufferSize { get; set; }
 
-        public void Stop()
-        {
+		public string ServerBanner { get; set; }
 
-        }
+		public TimeSpan ReadTimeout { get; set; }
 
-        #endregion
+		public TimeSpan WriteTimeout { get; set; }
 
-        #region Private/Internal Methods
-        private void VerifyState(HttpServerState state)
-        {
+		public TimeSpan ShutdowTimeout { get; set; }
 
-        }
+		internal HttpServerUtility ServerUtility { get; private set; }
 
-        private void StopClients()
-        {
+		internal HttpTimeoutManager TimeoutManager { get; private set; }
 
-        }
+		#endregion
 
-        private void BeginAcceptTcpClient()
-        {
+		#region Constructor
 
-        }
+		public HttpServer ()
+		{
+			Port = 0;
+			EndPoint = new IPEndPoint (IPAddress.Loopback, Port);
+			ReadBufferSize = WriteReadBufferSize;
+			WriteBufferSize = WriteReadBufferSize;
+			ShutdowTimeout = TimeSpan.FromSeconds (ShutdownTimeout);
+			ReadTimeout = TimeSpan.FromMinutes (WriteReadTimeOut);
+			WriteTimeout = TimeSpan.FromMinutes (WriteReadTimeOut);
+			ServerBanner = string.Format ("PHttp/{0}", GetType ().Assembly.GetName ().Version);
+		}
 
-        private void AcceptTcpClientCallback(IAsyncResult asyncResult)
-        {
+		public HttpServer(int port) : this(){
+			Port = port;
+			EndPoint.Port = port;
+		}
 
-        }
+		#endregion
 
-        private void RegisterClient(HttpClient client)
-        {
+		#region Public Methods
 
-        }
+		public void Start ()
+		{
+			if (_state != HttpServerState.Stopped)
+				return;
 
-        internal void UnregisterClient(HttpClient client)
-        {
+			State = HttpServerState.Starting;
+			Console.WriteLine ("Starting server at: " + EndPoint);
+			TimeoutManager = new HttpTimeoutManager (this);
+			var listener = new TcpListener (EndPoint);
 
-        }
-        #endregion
+			try{
+				listener.Start();
+				_tcpListener = listener;
+				ServerUtility = new HttpServerUtility();
+				Console.WriteLine("Server is running at: " + EndPoint);				
+			}
+			catch (Exception e){ 
+				State = HttpServerState.Stopped;
+				throw new PHttpException ("Server failed to start", e);
+			}
 
-        #region Implemented Methods
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+			State = HttpServerState.Started;
+			BeginAcceptTcpClient ();
+		}
 
-        #endregion
-    }
+		public void Stop ()
+		{
+
+		}
+
+		#endregion
+
+		#region Private/Internal Methods
+
+		private void VerifyState (HttpServerState state)
+		{
+			if (_disposed)
+				throw new ObjectDisposedException (this.GetType ().Name);
+
+			if (_state != state) {
+				throw new InvalidOperationException ("Expected server to be in the state");
+			}
+		}
+
+		private void StopClients ()
+		{
+
+		}
+
+		private void BeginAcceptTcpClient ()
+		{
+			var listener = _tcpListener;
+			if (listener == null)
+				return;
+
+			listener.BeginAcceptTcpClient (AcceptTcpClientCallback, null);
+		}
+
+		private void AcceptTcpClientCallback (IAsyncResult asyncResult)
+		{
+
+		}
+
+		private void RegisterClient (HttpClient client)
+		{
+
+		}
+
+		internal void UnregisterClient (HttpClient client)
+		{
+
+		}
+
+		protected virtual void OnStateChanged (EventArgs args)
+		{
+			if (StateChanged != null) {
+				StateChanged.Invoke (this, args);
+			}
+		}
+
+		#endregion
+
+		#region Implemented Methods
+
+		public void Dispose ()
+		{
+			if (_disposed)
+				return;
+			
+			if (_state == HttpServerState.Started)
+				Stop ();
+			
+			if (_clientsChangedEvent != null) {
+				_clientsChangedEvent.Dispose ();
+				_clientsChangedEvent = null;
+			}
+
+			_disposed = true;
+		}
+
+		#endregion
+	}
 }
---
+++ src/PHttp/HttpServerUtility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Web;
+
+namespace PHttp
+{
+	public class HttpServerUtility
+	{
+		internal HttpServerUtility ()
+		{
+		}
+		public string MachineName
+		{
+			get { return Environment.MachineName;}
+		}
+
+		public string HtmlEncode(string value)
+		{
+			return HttpUtility.HtmlEncode(value);
+		}
+
+		public string HtmlDecode(string value)
+		{
+			return HttpUtility.HtmlDecode(value);
+		}
+
+		public string UrlEncode(string text)
+		{
+			return Uri.EscapeDataString(text);
+		}
+
+		public string UrlDecode(string text)
+		{
+			return UrlDecode(text, Encoding.UTF8);
+		}
+
+		public string UrlDecode(string text, Encoding encoding)
+		{
+			if (encoding == null)
+				throw new ArgumentNullException("encoding");
+
+			return HttpUtility.UrlDecode(text, encoding);
+		}
+	}
+}
+
